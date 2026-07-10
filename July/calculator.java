1df4e904652d0e86f0af5f1a3f563637c452e222
k1799 <gkannan1799@gmail.com>
2026-07-10 19:40:33 +0530

    program

---
+++ July/calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package July;
+
+
+public class calculator {
+    public static void main(String[] args) {
+        int a =10;
+int b=10;
+        System.out.println(a+b);
+    }
+  
+
+}
